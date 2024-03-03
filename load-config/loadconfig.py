72ed3fd35d886417a3d3f0afdf6d76487efa2bd0
Brandon Phillips <phizzl3@gmail.com>
2024-03-03 17:01:10 -0600

    Update loadconfig
    Breaks apart and updates functions

--- load-config/loadconfig.py
+++ load-config/loadconfig.py
@@ -4,37 +4,34 @@ import json
 from pathlib import Path
 
 
-def loadjson(json_path: Path, default_data=None):
-    """Checks to see if a json file exists at json_path, and returns a
-    python data type of the contents read from the json file. If the
-    json file doesn't exist, outputs json file using the python data
-    type (json compatible) optionally passed as default_data, then 
-    loads from the file.
+def load_config(json_path: Path, default_data: any = None):
+    json_path = __verify_path_object(json_path)
+    __verify_or_write_json(json_path, default_data)
+    return __load_data_from_json(json_path)
 
-    Args:
-        json_path (Path): pathlib.Path pointing to json file location.
-        default_data (any, optional): python data type (json compatible) to 
-        output to json file. Defaults to None.
 
-    Returns:
-        any: python data type read from json data.
-    """
-    # Convert to Path if passed as str
+def __verify_path_object(json_path: Path) -> Path:
     if not isinstance(json_path, Path):
         json_path = Path(json_path)
-    # Checks to see if the target file exists and creats
-    # the folder path if it doesn't exist.
+    return json_path
+
+
+def __write_default_data(json_path: Path, default_data: any) -> None:
+    with open(json_path, "w", encoding="utf-8") as json_file:
+        json.dump(default_data, json_file, indent=2)
+
+
+def __verify_or_write_json(json_path: Path, default_data: any = None) -> None:
     if not json_path.exists():
         if not json_path.parent.is_dir():
             json_path.parent.mkdir(parents=True)
 
         if default_data:
-            # Writes the python data to output json file.
-            with open(json_path, "w", encoding="utf-8") as json_file:
-                json.dump(default_data, json_file, indent=2)
+            __write_default_data(json_path, default_data)
         else:
             input(f"\n File not found:\n {json_path}")
 
-    # Load the data from the json file at the target location.
+
+def __load_data_from_json(json_path: Path) -> any:
     with open(json_path, "r", encoding="utf-8") as json_file:
         return json.load(json_file)
