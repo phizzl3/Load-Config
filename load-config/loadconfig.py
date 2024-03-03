5f29b892c3958b24abf4dca3201a59edde15ae8b
Brandon Phillips <phizzl3@gmail.com>
2024-03-03 16:11:53 -0600

    Rename files and folders

--- load-config/loadconfig.py
+++ load-config/loadconfig.py
@@ -1,23 +1,4 @@
-"""
-
-Checks to see if a json file exists at json_path, and returns a 
-python data type of the contents read from the json file. If the 
-json file doesn't exist, outputs json file using the python data 
-type (json compatible) optionally passed as default_data, then 
-loads from the file.
-
-Args:
-    json_path (Path): pathlib.Path pointing to json file location.
-    
-    default_data (any, optional): python data type (json compatible) 
-    to output to json file. Defaults to None.
-
-Returns:
-    any: python data type read from json data.
-    
-"""
-
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 
 import json
 from pathlib import Path
