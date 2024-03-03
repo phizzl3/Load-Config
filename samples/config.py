ac24c5b3de1b2bd6712849092d954685c22c36f0
Brandon Phillips <phizzl3@gmail.com>
2024-03-03 17:01:28 -0600

    Update config.py sample

--- samples/config.py
+++ samples/config.py
@@ -1,16 +1,14 @@
 """
-
-Load (use defaults first if not found) json data from
-file, and use that data to generate a settings file for import.
-
+Load (use defaults first if not found) config json data from
+file, and use that data to generate a config.py file for import.
 """
 
 from pathlib import Path
-from loadjsondata import loadjson
 
+from .loadconfig import load_config
 
 # json file location
-JSON = Path().home() / "PyAppFiles" / "Combine PDFs" / "settings.json"
+JSON = Path().home() / "PyAppFiles" / "My Application" / "config.json"
 
 # Default values for json data
 DEFAULTS = {
@@ -19,20 +17,20 @@ DEFAULTS = {
     "output file name": "_Merged_PDF.pdf",
 }
 
-# Load (or set defaults) settings json data
-data = loadjson(JSON, default_data=DEFAULTS)
+# Load (or set defaults) config json data
+data = load_config(json_path=JSON, default_data=DEFAULTS)
 
 # Do stuff with data
 important_number = data["random number"] ** 2
 working_dir = data["working folder name"][3:]
 file_name = data["output file name"].lower()
 
-# Formatted settings data (Import this)
-SETTINGS = {
+# Formatted config data (Import this)
+CONFIG = {
     "number": important_number,
     "merge folder": Path().home() / working_dir,
-    "output file": file_name
+    "output file": file_name,
 }
 
-# Or just load the json data (Import this)
-SETTINGS_2 = loadjson(JSON, default_data=DEFAULTS)
+# Or just load the config json data (Import this)
+CONFIG = load_config(json_path=JSON, default_data=DEFAULTS)
