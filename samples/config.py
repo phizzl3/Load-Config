35ef9f895631b1c1318bcdcb9910684aee8692d0
B <phizzl3@gmail.com>
2025-03-13 12:06:36 -0500

    Update sample configuration module with enhanced documentation and TODOs for customization

--- samples/config.py
+++ samples/config.py
@@ -1,16 +1,23 @@
 """
-Load (use defaults first if not found) config json data from
-file, and use that data to generate a config.py file for import.
+* This is a sample configuration module for loading JSON data *
+
+This module loads configuration data from a JSON file,
+applies default values if the file is not found,
+and processes the data to generate a configuration dictionary for import.
+
+Modify this module to use loadconfig.py to suit your needs in loading your
+configuration data.
 """
 
 from pathlib import Path
 
+# TODO Import the load_config function from its relative path
 from .loadconfig import load_config
 
-# json file location
+# TODO Update to your desired JSON file location
 JSON = Path().home() / "PyAppFiles" / "My Application" / "config.json"
 
-# Default values for json data
+# TODO Update to your Default values for your JSON data
 DEFAULTS = {
     "random number": 20,
     "working folder name": "Combine_PDFs",
@@ -20,17 +27,17 @@ DEFAULTS = {
 # Load (or set defaults) config json data
 data = load_config(json_path=JSON, default_data=DEFAULTS)
 
-# Do stuff with data
+# TODO Work with the JSON data to generate your configuration values
 important_number = data["random number"] ** 2
 working_dir = data["working folder name"][3:]
 file_name = data["output file name"].lower()
 
-# Formatted config data (Import this)
+# TODO Update this formatted config data (Import this)
 CONFIG = {
     "number": important_number,
     "merge folder": Path().home() / working_dir,
     "output file": file_name,
 }
 
-# Or just load the config json data (Import this)
-CONFIG = load_config(json_path=JSON, default_data=DEFAULTS)
+# TODO Or just load the config JSON data without modification (OR Import this)
+CONFIG2 = load_config(json_path=JSON, default_data=DEFAULTS)
