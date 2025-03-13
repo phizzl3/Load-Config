75ee37fb6481772c297aea234f9b49d04cb5a4dd
B <phizzl3@gmail.com>
2025-03-13 11:09:13 -0500

    Refactor load_config functions to improve type hints and error handling

--- load-config/loadconfig.py
+++ load-config/loadconfig.py
@@ -1,10 +1,12 @@
 __version__ = "1.1.0"
 
 import json
+import time
 from pathlib import Path
+from typing import Any, Optional
 
 
-def load_config(json_path: Path, default_data: any = None):
+def load_config(json_path: Path, default_data: Optional[Any] = None) -> Any:
     json_path = __verify_path_object(json_path)
     __verify_or_write_json(json_path, default_data)
     return __load_data_from_json(json_path)
@@ -16,12 +18,12 @@ def __verify_path_object(json_path: Path) -> Path:
     return json_path
 
 
-def __write_default_data(json_path: Path, default_data: any) -> None:
+def __write_default_data(json_path: Path, default_data: Any) -> None:
     with open(json_path, "w", encoding="utf-8") as json_file:
         json.dump(default_data, json_file, indent=2)
 
 
-def __verify_or_write_json(json_path: Path, default_data: any = None) -> None:
+def __verify_or_write_json(json_path: Path, default_data: Optional[Any] = None) -> None:
     if not json_path.exists():
         if not json_path.parent.is_dir():
             json_path.parent.mkdir(parents=True)
@@ -29,9 +31,10 @@ def __verify_or_write_json(json_path: Path, default_data: any = None) -> None:
         if default_data:
             __write_default_data(json_path, default_data)
         else:
-            input(f"\n File not found:\n {json_path}")
+            print(f"\n File not found:\n {json_path}")
+            time.sleep(5)
 
 
-def __load_data_from_json(json_path: Path) -> any:
+def __load_data_from_json(json_path: Path) -> Any:
     with open(json_path, "r", encoding="utf-8") as json_file:
         return json.load(json_file)
